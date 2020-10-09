ab5d582d49d1346cabc72cf0912e5a71ec4b6711
raulsposito <raulsposito@gmail.com>
2020-10-09 15:44:34 -0300

    add login get and post route and signup get route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :bookings
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
 end
