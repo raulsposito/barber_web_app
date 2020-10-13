64a847eb9aa120e42f26a522e42393feda0fa5b0
raulsposito <raulsposito@gmail.com>
2020-10-13 00:36:04 -0300

    fix create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-        session[:user_id] = params[:user_id]
-        redirect_to users_path
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            flash[:error] = "Wrong Password!"
+            redirect_to login_path
+        end
     end
 
     def destroy
