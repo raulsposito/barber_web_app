bd10a92663fe22ac3db2decebef6f1266db3203c
raulsposito <raulsposito@gmail.com>
2020-10-15 12:35:56 -0300

    add date to bookings and bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 class BookingsController < ApplicationController
+    before_action :authentication_required
+
+    def new 
+        @booking = Booking.new
+    end
+
+    def index
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_195200) do
+ActiveRecord::Schema.define(version: 2020_10_15_153151) do
 
   create_table "barbers", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_10_03_195200) do
     t.integer "barber_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "date"
   end
 
   create_table "comments", force: :cascade do |t|
