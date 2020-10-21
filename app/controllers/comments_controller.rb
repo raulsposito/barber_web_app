492c6e9f6b51bef4efa215077060977ec7003d7c
raulsposito <raulsposito@gmail.com>
2020-10-20 22:08:19 -0300

    add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+
+    def new 
+        @booking = Booking.find_by_id(params[:id])
+        @comment = @booking.build_comment
+    end
+
+    def create 
+        @comment = Comment.new(comment_params)
+        if @comment.save   
+            redirect_to comment_path(@comment)
+        else
+            render :new 
+        end
+    end
+
+    def show
+        authentication_required
+        @comment = Comment.find_by_id(params[:id])
+    end
+
+    private 
+
+    def user_params 
+        params.require(:comment).permit(:body, :user_id, :booking_id)
+    end
+
 end
