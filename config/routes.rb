c4fba3b61dded98b9578fcc650b9fa7acc289c9a
raulsposito <raulsposito@gmail.com>
2020-10-20 22:09:29 -0300

    nest comments routes under bookings resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'static#home'
   resources :comments
   resources :barbers
-  resources :bookings
+  resources :bookings do 
+    resources :comments, only: [:new, :index]
+  end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/login' => 'sessions#new'
