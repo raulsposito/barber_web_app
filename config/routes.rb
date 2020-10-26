685707aed2600a5211386b240cd87c2e551d9895
raulsposito <raulsposito@gmail.com>
2020-10-26 02:22:35 -0300

    add nested create to comments routes and index view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :barbers
   resources :bookings do 
-    resources :comments, only: [:new, :index]
+    resources :comments, only: [:new, :create, :index]
   end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
