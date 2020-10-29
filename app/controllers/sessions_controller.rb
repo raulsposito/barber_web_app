04650c7b740baa4e93fff486e4a25a5d6077661f
raulsposito <raulsposito@gmail.com>
2020-10-28 21:50:55 -0300

    add omniauth method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,11 +15,19 @@ class SessionsController < ApplicationController
     end
 
     def omniauth 
-        binding.pry
+        @user = User.find_or_create_by_google(auth)
+        session[:user_id] = @user.id 
+        redirect_to user_path(@user)
     end
 
     def destroy
         session.delete :user_id
         redirect_to '/'
+    end
+
+    private 
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
