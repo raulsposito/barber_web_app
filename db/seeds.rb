bd943bbee907ef089b94ebec9ab5b3c53a483cc0
raulsposito <raulsposito@gmail.com>
2020-11-08 01:17:32 -0300

    add another barber to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 User.create(name: 'Seed1', email: 'seed1@gmail.com', phone: '099123456', password: '123')
 
 Barber.create(name: 'Agustin', image_url: 'https://cdn4.iconfinder.com/data/icons/jobs-and-occupations-2/64/Barber-job-avatar-profession-occupation-hairdresser-salon-stylist-haircut-512.png', email: 'elagusel1@gmail.com')
+Barber.create(name: 'Leonardo', image_url: 'https://cdn3.iconfinder.com/data/icons/avarta/512/barber-hairdresser-user-avatar-man-512.png', email: 'leobarber123@gmail.com')
