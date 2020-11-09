f901842b678a47b06648c677a0a80dd8c678d2b6
raulsposito <raulsposito@gmail.com>
2020-11-08 23:36:47 -0300

    add scope method for next week bookings report

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,6 @@ class Booking < ApplicationRecord
     has_many :comments
 
     validates :date, presence: true
+    #validates :date, uniqueness: { scope: :datetime, message: "That day is taken, choose another one."}
+    scope :next_week, -> { where('date > ?', Time.now+7.days) }
 end
