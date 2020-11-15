ae3909ed74461493caf9cba2e10c5b2f9bc58c17
raulsposito <raulsposito@gmail.com>
2020-11-15 17:42:12 -0300

    add accepts nested attributes for comments

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,8 @@ class Booking < ApplicationRecord
     belongs_to :barber
     has_many :comments
 
+    accepts_nested_attributes_for :comments
     validates :date, presence: true
     #validates :date, uniqueness: { scope: :datetime, message: "That day is taken, choose another one."}
-    scope :next_week, -> { where('date > ?', Time.now+7.days) }
+    scope :next_week, -> { where('date < ?', Time.now+7.days) }
 end
