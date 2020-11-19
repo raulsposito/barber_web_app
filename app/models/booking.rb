0c47de9aae49c6b358e5eb28831a04748fdb399a
raulsposito <raulsposito@gmail.com>
2020-11-19 12:04:07 -0300

    add validations for booking dates not to be in the past

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,20 +5,12 @@ class Booking < ApplicationRecord
 
     accepts_nested_attributes_for :comments
     validates :date, presence: true
+    validate :date_cannot_be_in_the_past
+
+    def date_cannot_be_in_the_past
+        errors.add(:date, "can't be in the past") if date < Date.today
+    end
     #validates :date, uniqueness: { scope: :date, message: "That day is taken."}
     scope :next_week, -> { where('date < ?', Time.now+7.days) }
-
-
-    def cancel_booking
-        booking.status = false
-    end
-
-    def self.active?
-        if 
-            self.status = true 
-        else
-            false
-        end
-    end
 
 end
