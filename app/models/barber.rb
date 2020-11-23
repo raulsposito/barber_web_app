dc68ec53e4777bb31d41b4ec480b0c5974a9c5d6
raulsposito <raulsposito@gmail.com>
2020-11-22 23:44:53 -0300

    add accepts nested attributes for :slots

--- app/models/barber.rb
+++ app/models/barber.rb
@@ -2,6 +2,7 @@ class Barber < ApplicationRecord
 
     has_many :bookings 
     has_many :slots
+    accepts_nested_attributes_for :slots
     has_many :users ,through: :bookings
     has_many :comments ,through: :bookings
 end
